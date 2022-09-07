222d8ecf1482413fc41b82e7302d9ae8d289f306
wise-man-distracted <magewelle@live.com>
2022-09-07 13:56:17 -0300

    fiz alterações malucas que eu não quero que sejam colocadas no projeto por agora

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Qualquer coisa')
+const fs = require('fs')
+
+
+fs.writeFileSync("arquivoComplexo.js", "console.log(15646546541)")
+
+
+let numeroDoido = 15 * 45 + 55 / 52 % 5
+
+console.log(numeroDoido)
